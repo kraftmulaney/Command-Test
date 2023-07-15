3542629fb48056fc8fa29bd319301dea2d469069
Ido <idoben2@hotmail.com>
2023-07-15 11:56:07 -0700

    Implement rest of MotorSimOutput

--- src/main/java/frc/robot/simulation/motor/MotorSimOutput.java
+++ src/main/java/frc/robot/simulation/motor/MotorSimOutput.java
@@ -1,18 +1,27 @@
 package frc.robot.simulation.motor;
 
+import com.revrobotics.RelativeEncoder;
 import frc.robot.simulation.framework.SimOutputDoubleInterface;
+import frc.robot.subsystems.RelativeEncoderSim;
 
 /**
  * Helper class to implement OutputDoubleInterface.
  */
 public class MotorSimOutput implements SimOutputDoubleInterface {
+  private final RelativeEncoderSim m_encoderRealWrapper;
+
   /**
    * Constructor.
    */
-  MotorSimOutput() {
+  MotorSimOutput(RelativeEncoder encoderReal) {
+    // Create a "sim" wrapper of the Robots real encoder. The sim wrapper
+    // is later used to write to the real encoder
+    m_encoderRealWrapper = new RelativeEncoderSim(encoderReal);
   }
 
   @Override
   public void setOutput(Double output) {
+    // Sets the encoder position in rotations.
+    m_encoderRealWrapper.setPosition(output);
   }
 }
